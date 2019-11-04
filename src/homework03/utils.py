8ca51f73021b0c36194f7634b1c72d79f09e716e
Louis Merlin <louis_merlin1@yahoo.fr>
2019-11-04 15:47:56 +0100

    Continue working on hw3

--- src/homework03/utils.py
+++ src/homework03/utils.py
@@ -1,7 +1,10 @@
 import imp
 
-def get_parameters():
-    f = open("247565-parameters.txt")
+def get_parameters(test=False):
+    if test:
+        f = open("100000-parameters.txt")
+    else:
+        f = open("247565-parameters.txt")
     d = imp.load_source('d', '', f)
     f.close()
     return d
