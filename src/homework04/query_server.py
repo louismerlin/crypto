931dc13117b19a89282d9f3ae280a84183b80860
Louis Merlin <louis_merlin1@yahoo.fr>
2019-11-17 15:25:41 +0100

    Create homework 4 skeleton

---
+++ src/homework04/query_server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3 
+import socket
+
+def connect_server(server_name, port, message):
+    server = (server_name, int(port)) #calling int is required when using Sage
+    s = socket.create_connection(server)
+    s.send(bytes(message + "\n", "ASCII"))
+    response=""
+    while True: #data might come in several packets, need to wait for all of it
+        data = s.recv(1024)
+        data = data.decode("ASCII")
+        if len(data) == 0:
+            break
+        if data[-1] == '\n': 
+            response += data[:-1]  
+            break
+        response += data
+    s.close()
+    return response
+
+
+def encryption_query(sciper, pt):
+    server_name = "lasecpc25.epfl.ch"
+    port = "5559"
+    message = sciper + " " + pt
+    response = connect_server(server_name, port, message)
+    #print(response)
+    return response
